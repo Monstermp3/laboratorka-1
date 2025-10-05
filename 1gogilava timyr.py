be6d1472532ee1fb93eda0d246928fdf04649a44
timyrka <timyr200088@mail.ru>
2025-10-05 20:11:30 +0300

    Initial commit

---
+++ 1gogilava timyr.py
@@ -0,0 +1,12 @@
+x = int(input("55"))
+for i in range(1, x + 1):
+        print(i)
+
+x = int(input('5'))
+y = int(input('10'))
+if x > y:
+        print(x)
+elif x < y:
+        print(y)
+else:
+        print(x)
